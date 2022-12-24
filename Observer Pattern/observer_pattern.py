521301022b24f717333e1bd7a6893852f187aa81
Honko-o <98553562+Honko-o@users.noreply.github.com>
2022-12-24 22:19:54 +0300

    Add Observe Pattern Folder with Python file

---
+++ Observer Pattern/observer_pattern.py
@@ -0,0 +1,93 @@
+from abc import ABC
+
+class Subject:
+	"""Represents what is being observed"""
+
+	def __init__(self):
+		"""create an empty observer list"""
+		self._observers = []
+
+	def notify(self, modifier = None):
+		"""Alert the observers"""
+		for observer in self._observers:
+			if modifier != observer:
+				observer.update(self)
+
+	def registerObserver(self, observer):
+		"""If the observer is not in the list,
+		append it into the list"""
+		if observer not in self._observers:
+			self._observers.append(observer)
+
+	def removeObserver(self, observer):
+		"""Remove the observer from the observer list"""
+		try:
+			self._observers.remove(observer)
+		except ValueError:
+			pass
+
+class Observer(ABC):
+	def update(self, subject):
+		pass
+
+# Concrete Subject
+class Data(Subject):
+	"""monitor the object"""
+	def __init__(self, name =''):
+		Subject.__init__(self)
+		self.name = name
+		self._data = 0
+
+	@property
+	def data(self):
+		return self._data
+
+	@data.setter
+	def data(self, value):
+		self._data = value
+		self.notify()
+
+
+class HexViewer(Observer):
+	"""updates the Hexviewer"""
+	def update(self, subject):
+		print('HexViewer: Subject {} has data 0x{:x}'.format(subject.name, subject.data))
+
+class OctalViewer(Observer):
+	"""updates the Octal viewer"""
+	def update(self, subject):
+		print('OctalViewer: Subject' + str(subject.name) + 'has data '+str(oct(subject.data)))
+
+
+class DecimalViewer(Observer):
+	"""updates the Decimal viewer"""
+	def update(self, subject):
+		print('DecimalViewer: Subject % s has data % d' % (subject.name, subject.data))
+
+"""main function"""
+
+if __name__ == "__main__":
+
+	"""provide the data"""
+
+	obj1 = Data('Data 1')
+	obj2 = Data('Data 2')
+
+	view1 = DecimalViewer()
+	view2 = HexViewer()
+	view3 = OctalViewer()
+
+	obj1.registerObserver(view1)
+	obj1.registerObserver(view2)
+	obj1.registerObserver(view3)
+
+	obj2.registerObserver(view1)
+	obj2.registerObserver(view2)
+	obj2.registerObserver(view3)
+
+	print('*' * 50)
+
+	obj1.removeObserver(view1)
+
+	obj1.data = 10
+	# obj2.data = 15
